9374cc0241bdeb125ef96b7255f1394fe3480e12
Mahmudul Hasan <mahmudnotes@outlook.com>
2021-02-13 23:39:39 +0600

    worked with mutable var

--- ConsoleFS/Program.fs
+++ ConsoleFS/Program.fs
@@ -1,4 +1,5 @@
 ﻿open System
+open bind_obj
 
 let Hello() =
     printf "Enter your name: "
@@ -7,5 +8,17 @@ let Hello() =
 
     printfn "Hello %s" name
 
-Hello()
+    printfn "PI : %f" 3.141592
+
+let Pi() = 
+    let big_pi = 3.1415926535897932384626433832M
+    printfn "PI : %M" big_pi
+
+let DynamicPadding() =
+    printfn "%5s %5s" "a" "b"
+    printfn "%*s" 12 "Hi"
+
+DynamicPadding()
+bind()
+
 Console.ReadKey() |> ignore
---
+++ ConsoleFS/bind_obj.fs
@@ -0,0 +1,11 @@
+﻿module bind_obj
+open System
+
+let bind() = 
+    let mutable weight = 175
+    weight <- 170
+    printfn "Weight: %i" weight
+
+    let change_me = ref 10
+    change_me := 20
+    printfn "Change: %i" ! change_me
